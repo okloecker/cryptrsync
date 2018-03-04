5a60e6f8d946f70a5f64f1263ff4f6ba2e1d4a11
Olaf Kloecker <okl@posteo.de>
2018-03-04 16:56:27 +0000

    initial commit

---
+++ src/cryptrsync.sh
@@ -0,0 +1,192 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o errexit
+
+# script to watch a directory and if any changes occur, sync this with a remote,
+# either using rclone or rsync
+# There is a distinction between dir to watch and local dir to allow use of gocrypt:
+# inotifywait can't watch the encrypted view, only the original dir
+
+# call with parameters:
+# ./cryptrsync.sh 
+# [--id=<uniqueID>] 
+# [--dry-run]
+# --method=<rsync|rclone> 
+# --plaindir=<unencrypted dir> --syncdir=<encrypted dir> --url=<rsync or rclone url>
+# --log=<logfile>
+
+readonly rclone=`which rclone`
+rcloneopts="-v --stats=20s"
+rsyncopts="--archive --stats --delete --progress"
+readonly delay="15s"
+readonly popupduration=5000
+readonly configdir=$HOME/.config/cryptrsync
+readonly DATE='date +%Y-%m-%d_%H:%M:%S'
+LOG=${configdir}/log_cryptrsync
+id="cryptrsync"
+method=${1}
+plaindir=${2}
+syncdir=${3}
+url=${4}
+
+# whether to use rclone (or rsync) (1=rclone, 0=rsync)
+use_rclone=0
+
+function echo_log {
+  echo `$DATE`" $1" |tee -a ${LOG}
+}
+
+alert(){
+   notify-send -u critical -t $popupduration " cryptrsync  `$DATE`   ${@}  "
+}
+alert_fail(){
+   notify-send -u critical -t $popupduration " cryptrsync  `$DATE`   ${@}  " --icon=${icon}
+}
+
+parseoptions() {
+  for arg in $*; do
+     # Split arg on "=". It is OK to have an "=" in the value, but not
+     # the key.
+     key=$(   echo ${arg} | cut --delimiter== --fields=1  )
+     value=$( echo ${arg} | cut --delimiter== --fields=2- )
+
+     case ${key} in
+         "--id")
+           id="${value}"
+           ;;
+         "--method")
+           if [ "${value}" = "rclone" ] ; then
+            use_rclone=1
+          fi
+          ;;
+         "--dry-run")
+           rcloneopts="${rcloneopts} --dry-run"
+           rsyncopts="${rsyncopts} --dry-run";;
+         "--plaindir")
+           plaindir=${value};;
+         "--syncdir")
+           syncdir=${value}
+           ;;
+         "--url")
+           url=${value}
+          ;;
+         "--log")
+           LOG="${value}"
+           ;;
+         *)
+          echo_log "Unrecognised option ${key}"
+              exit 1
+     esac
+  done
+}
+
+mountgocrypt() {
+  # mount syncdir if necessary
+  if [ ! -e "${syncdir}/gocryptfs.diriv" ] ; then
+    echo_log "============ Reverse mounting crypted dir ${syncdir}"
+    echo_log "Enter gocrypt password for ${plaindir} - ${syncdir}:"
+    gocryptfs -reverse -q "${plaindir}" "${syncdir}" 
+  else
+    echo_log "============ Crypted dir already mounted: ${syncdir}"
+  fi
+}
+
+function finish {
+    if [ -n ${waitpid:-''} ] ; then
+    echo_log "--- Cleaning up, killing $waitpid and removing $waitpidfile and ${changedfile}"
+    echo
+    rm -rf "${changedfile}" $waitpidfile
+    kill $waitpid
+  else
+    echo_log "============ Exiting"
+  fi
+}
+trap finish EXIT #SIGINT SIGTERM
+
+lock() {
+  local prefix=$1
+  local fd=${2:-$LOCK_FD}
+  local lock_file=$LOCKFILE_DIR/$prefix.lock
+
+  # create lock file
+  eval "exec $fd>$lock_file"
+
+  # acquier the lock
+  #flock --wait $LOCKWAIT $fd \
+  flock $fd \
+    && return 0 \
+    || return 1
+}
+
+unlock() {
+  local fd=${1:-$LOCK_FD}
+  flock -u $fd && return 0 || return 1
+}
+
+readchanges () {
+  f="$1"
+  lock changesfile || echo_log "Can\'t lock in readchanges()"
+  echo_log "============ NOTIFIED CHANGE FOR $f"
+  echo $f >> ${changedfile}
+  unlock || echo_log "--- Couldn\'t remove lock"
+}
+
+startinotifyw() {
+  ( inotifywait -m "${plaindir}" -r -e close_write,create,delete --format %w%f & echo $! >&3 ) 3>$waitpidfile | egrep --line-buffered -v ${ignore} | 
+    while read f ; do readchanges "$f" ; done &
+  waitpid=$(<$waitpidfile)
+  echo_log "PID of inotifywait ${waitpid}"
+  echo_log "CHANGEDFILE ${changedfile}"
+  echo_log "PIDFILE ${waitpidfile}"
+}
+
+sync () {
+  local force=${1:-0} 
+
+  lock changesfile || echo_log "Can\'t lock in while"
+  if [ $force = 1 ] || [ -s ${changedfile} ] ; then
+    sort ${changedfile} | uniq | while read f ; do 
+      echo_log "============ SYNCING $f"
+    done
+
+    echo_log "......................................................."
+    if [ $use_rclone = 1 ] ; then
+      echo_log "============ CALLING rclone"
+      ${rclone} sync ${rcloneopts} "${syncdir}" "${url}" 2>&1 | tee -a "${LOG}" &&  alert "OK ${id}" || alert_fail "ERR ${id}" | tee -a "${LOG}"
+    else
+      echo_log "============ CALLING rsync at with options ${rsyncopts}"
+      rsync ${rsyncopts} ${syncdir}/ "${url}" 2>&1 | tee -a "${LOG}" && alert "OK ${id}" || alert_fail "ERR ${id}"
+    fi
+    rm -f ${changedfile}
+  fi
+  unlock || echo_log "--- Couldn\'t remove lock"
+}
+
+main() {
+  parseoptions $@
+
+  readonly icon=/usr/share/icons/Adwaita/256x256/status/dialog-error.png
+  readonly ignore="\.~lock\.|\.sw.?|~$"
+  readonly changedfile=`mktemp --suffix=_${id}_sync`
+  readonly waitpidfile=`mktemp --suffix=_${id}_sync`
+
+  readonly LOCKFILE_DIR=/tmp
+  readonly LOCK_FD=9
+  readonly LOCKWAIT=120
+
+  mountgocrypt
+
+  startinotifyw
+
+  sync 1
+
+  while true ; do
+    sleep $delay
+
+#    echo_log " [$$] checking ${id} ${plaindir}"
+    sync
+
+  done
+}
+
+main $@
---
+++ src/cryptrsyncassist.sh
@@ -0,0 +1,164 @@
+#!/usr/bin/env bash
+set -o nounset
+set -o errexit
+
+# Assistant for cryptrsync.sh that reads the command line parameters from a
+# configuration file ($HOME/.config/cryptrsync/autostarts
+# Config file must have 5 elements:
+# <id>;<method>;<plaindir>;<cryptdir>;<url>
+#
+# Example config entries:
+#Private;rsync;/home/user/Private;/home/user/Private_gocrypt;user@example.com:/home/user/Private_gocrypt
+#Media;rclone;/home/user/Bigfiles;/home/user/Bigfiles_gocrypt;gmailuser:gocrypt/Bigfiles_gocrypt
+#
+# Call with the ID defined in the configuration file.
+# ./cryptrsyncassist.sh 
+# --sync=<id>
+# [--dry-run] 
+# --log=<logfile>
+#
+# List IDs: --showconfig
+
+cd `dirname $0`
+readonly cryptrsyncprog=./cryptrsync.sh
+readonly configdir=$HOME/.config/cryptrsync
+readonly autostartsfile=${configdir}/autostarts
+declare -A ids
+declare -A methods
+declare -A plaindirs
+declare -A syncdirs
+declare -A urls
+
+dryrun=""
+idtosync=""
+showconfig=0
+
+readonly DATE='date +%Y-%m-%d_%H:%M:%S'
+# log file
+LOG=${configdir}/log
+logdir=`dirname "${LOG}"`
+function echo_log {
+  local f=${LOG}_${1}
+  echo `$DATE`" $2" |tee -a ${f}
+}
+
+loadConfig() {
+  if [ -e "${autostartsfile}" ] ; then
+    echo_log ""  "============ Reading config from ${autostartsfile}"
+    local lineNo=1
+    while read line || [[ -n "$line" ]]; do
+      IFS=\; read id method ldir sdir rurl <<< "${line}"
+
+      if [ -z "${id}" ] || [ -z "${method}" ] || [ -z "${ldir}" ] || [ -z "${sdir}" ] || [ -z "${rurl}" ] ; then
+        echo_log ""  "============ incorrect config file in line ${lineNo}"
+        exit 1
+      fi
+
+      # ignore lines starting with "#"
+      [ ${id#"#"} != ${id} ] && continue
+
+      ids[${id}]="${id}"
+      methods[${id}]="${method}"
+      eval plaindirs[${id}]="${ldir}"
+      eval syncdirs[${id}]="${sdir}"
+      urls[${id}]="${rurl}"
+
+      ((lineNo++))
+    done < "${autostartsfile}"
+
+  else 
+    echo `date +"%Y-%m-%d %H:%M:%S"` "============ No config file ${autostartsfile}"
+    exit 2
+  fi
+}
+
+mountgocrypt() {
+  local id=${1:-""} 
+  # mount syncdir if necessary
+  if [ -n "${id}" ] ; then
+    local syncdir=${syncdirs[${id}]}
+    local plaindir=${plaindirs[${id}]}
+    if [ ! -e "${syncdir}/gocryptfs.diriv" ] ; then
+      echo_log "${id}"  "============ Reverse mounting crypted dir ${syncdir}"
+      echo_log "${id}"  "Enter gocrypt password for ${plaindir} - ${syncdir}:"
+      gocryptfs -reverse -q "${plaindir}" "${syncdir}" 
+    else
+      echo_log "${id}"  "============ Crypted dir already mounted: ${syncdir}"
+    fi
+  fi
+}
+
+function finish {
+  echo_log ""  "============ Exiting"
+}
+trap finish EXIT
+
+parseoptions() {
+  for arg in $*; do
+     # Split arg on "=". It is OK to have an "=" in the value, but not
+     # the key.
+     key=$(   echo ${arg} | cut --delimiter== --fields=1  )
+     value=$( echo ${arg} | cut --delimiter== --fields=2- )
+
+     case ${key} in
+         "--dry-run")
+           dryrun="--dry-run"
+           ;;
+         "--sync")
+           idtosync="${value}"
+           ;;
+         "--showconfig")
+           showconfig=1
+           ;;
+         "--log")
+           LOG="${value}"
+           logdir=`dirname "${LOG}"`
+           ;;
+         *)
+          echo `date +"%Y-%m-%d %H:%M:%S"` "Unrecognised option ${key}"
+          exit 1
+     esac
+  done
+}
+
+main() {
+  parseoptions $@
+
+  [ -d "${logdir}" ] || mkdir "${logdir}"
+
+  loadConfig 
+
+  if [ ${showconfig} = 1 ] ; then
+    echo `date +"%Y-%m-%d %H:%M:%S"` "============ Config:"
+    for id in "${!ids[@]}"; do
+      echo "[${id}]"
+      echo "          Plain dir ${plaindirs[${id}]}"
+      echo "          Synced dir ${syncdirs[${id}]}"
+      echo "          Remote Url ${urls[${id}]}"
+      echo "          Method ${methods[${id}]}"
+    done
+    exit
+  fi
+
+  if [ -z "${ids[${idtosync}]:-""}" ] ; then
+    echo `date +"%Y-%m-%d %H:%M:%S"` No config for [$id] found
+  fi
+
+  for id in "${!ids[@]}"; do
+    local id=${ids[${id}]}
+    local method=${methods[${id}]}
+    local syncdir=${syncdirs[${id}]}
+    local plaindir=${plaindirs[${id}]}
+    local url=${urls[${id}]}
+    if [ "${id}" = "${idtosync}" ] ; then
+      #mountgocrypt ${id}
+      echo_log ""  "===== Calling syncdir.sh for [$id]"
+      ${cryptrsyncprog} ${dryrun} --id="${id}" --method="${method}" --plaindir="${plaindir}" --syncdir="${syncdir}" --url="${url}" --log=${LOG}_${id}
+    fi
+
+  done
+
+#  echo "Assist: I AM $$"
+}
+
+main $@
